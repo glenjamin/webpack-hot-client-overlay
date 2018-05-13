96a1745a842896385a2fe0d80d71a727991c29d3
Glen Mailer <glen@stainlessed.co.uk>
2018-05-13 18:47:19 +0100

    Basic working version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # webpack-hot-client-overlay
+
+This is an early prototype.
+
+## Installation
+
+Install package
+```
+npm install --save-dev @glenjamin/webpack-hot-client-overlay
+```
+
+Add to `webpack.config.js`'s `entry` array.
+
+```
+entry: [
+  "./client",
+  mode === "development" && "@glenjamin/webpack-hot-client-overlay"
+].filter(Boolean),
+```
---
+++ index.js
@@ -0,0 +1,92 @@
+// added to build via DefinePlugin in webpack-hot-client/lib/plugins.js
+const options = __hotClientOptions__;
+
+const socket = require('webpack-hot-client/client/socket');
+
+socket(options, {
+  errors({ errors }) {
+    showProblems("errors", errors);
+  },
+
+  warnings({ warnings }) {
+    showProblems("warnings", warnings);
+  },
+
+  ok() {
+    clearOverlay();
+  }
+});
+
+var clientOverlay = document.createElement('div');
+clientOverlay.id = 'webpack-hot-client-overlay';
+var styles = {
+  background: 'rgba(0,0,0,0.85)',
+  color: '#E8E8E8',
+  lineHeight: '1.4',
+  whiteSpace: 'pre',
+  fontFamily: 'Menlo, Consolas, monospace',
+  fontSize: '13px',
+  position: 'fixed',
+  zIndex: 9999,
+  padding: '10px',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  overflow: 'auto',
+  dir: 'ltr',
+  textAlign: 'left'
+};
+for (var key in styles) {
+  clientOverlay.style[key] = styles[key];
+}
+
+var ansiHTML = require('ansi-html');
+var colors = {
+  reset: ['transparent', 'transparent'],
+  black: '181818',
+  red: 'E36049',
+  green: 'B3CB74',
+  yellow: 'FFD080',
+  blue: '7CAFC2',
+  magenta: '7FACCA',
+  cyan: 'C3C2EF',
+  lightgrey: 'EBE7E3',
+  darkgrey: '6D7891'
+};
+
+var Entities = require('html-entities').AllHtmlEntities;
+var entities = new Entities();
+
+function showProblems(type, lines) {
+  clientOverlay.innerHTML = '';
+  lines.forEach(function(msg) {
+    msg = ansiHTML(entities.encode(msg));
+    var div = document.createElement('div');
+    div.style.marginBottom = '26px';
+    div.innerHTML = problemType(type) + ' in ' + msg;
+    clientOverlay.appendChild(div);
+  });
+  if (document.body) {
+    document.body.appendChild(clientOverlay);
+  }
+}
+
+function clearOverlay() {
+  if (clientOverlay.parentNode) {
+    clientOverlay.parentNode.removeChild(clientOverlay);
+  }
+}
+
+function problemType (type) {
+  var problemColors = {
+    errors: colors.red,
+    warnings: colors.yellow
+  };
+  var color = problemColors[type] || colors.red;
+  return (
+    '<span style="background-color:#' + color + '; color:#fff; padding:2px 4px; border-radius: 2px">' +
+      type.slice(0, -1).toUpperCase() +
+    '</span>'
+  );
+}
